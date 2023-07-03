e98dc345d3791c8d0bc8f8d384ae9b7ac5c5f82c
PakiruChinoike <pakillbrine@gmail.com>
2023-07-03 09:02:38 -0300

    Add files via upload

--- CSE/script/login.js
+++ CSE/script/login.js
@@ -1,40 +1,59 @@
-let input = document.getElementById("user");
-let input2 = document.getElementById("senha");
+const input = document.querySelectorAll("[data-input]")
+const mensagem = document.querySelector("[data-mensagem]")
+
+let inputSenha = document.querySelector("#senha").value
+let inputUser = document.querySelector("#user").value
 
 const user = ["june", "pakiru", "gay"];
 const senha = ["facadamaldada", "pipokapakill", "sougay"];
 
 function loga() {
-    if (user.includes(input.value)) {
-        alert(input2.value===senha[user.indexOf(input.value)] ? "Senha correta" : "Senha incorreta") 
+    if (user.includes(toString(inputUser))) {
+        mensagem.setAttribute("data-mensagem", "visivel")
+        if (inputSenha==senha[user.indexOf(inputUser)]) {
+            mensagem.innerHTML = "Senha correta"
+        }
+        else {
+            mensagem.innerHTML = "Senha incorreta"
+        }
     }
     else {
-        alert("Usuário não detectado. Deseja criar uma nova conta?");
+        mensagem.setAttribute("data-mensagem", "visivel")
+        mensagem.innerHTML = "Usuário não detectado. Deseja criar uma nova conta?"
+        button.setAttribute("data-botao", "invisivel")
+
+        const buttonNo = document.createElement("button")
+        const noNode = document.createTextNode("Sim")
+        buttonNo.appendChild(noNode)
+        buttonNo.setAttribute("data-botao", "visivel")
+
+
+        const buttonYes = document.createElement("button")
+        const yesNode = document.createTextNode("Não")
+        buttonYes.appendChild(yesNode)
+        buttonYes.setAttribute("data-botao", "visivel")
+        buttonYes.addEventListener("click", () => {
+            cria()
+        })
+
+        document.querySelector(".botoes").appendChild(buttonYes)
+        document.querySelector(".botoes").appendChild(buttonNo)
     }
-    input.value = "";
-    input2.value = "";
 }
 
-function mostra() {
-    alert(input.value + input2.value);
-    input.value = "";
-    input2.value = "";
+function cria() {
+    
 }
 
-let button = document.getElementById("logar");
+const button = document.getElementById("logar")
 
 button.onclick = loga;
 
-input.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      document.getElementById("logar").click();
-    }
-});
-
-input2.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      document.getElementById("logar").click();
-    }
-});
+input.forEach((element) => {
+    element.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            loga();
+        }
+    })
+})
